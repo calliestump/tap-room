64c276c95bc3fb080e2559673766dcbe132d8844
Callie Stump <callie@stu.mp>
2021-02-23 19:48:37 -0500

    removes unnessary comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-// import KegList from "./KegList";
 import KegControl from "./KegControl";
 import {Container} from 'react-bootstrap';
 function App(){
